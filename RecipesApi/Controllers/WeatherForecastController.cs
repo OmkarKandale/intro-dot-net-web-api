4afb081285fb944e0aecf3c5d9ae2d0812a922cd
OmkarKandale <omkarkandale@gmail.com>
2021-08-29 14:07:27 +0530

    Added Authorizations with JWT

--- RecipesApi/Controllers/WeatherForecastController.cs
+++ RecipesApi/Controllers/WeatherForecastController.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.AspNetCore.Authorization;
 namespace RecipesApi.Controllers
 {
     [ApiController]
+    [Authorize]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
--- RecipesApi/Startup.cs
+++ RecipesApi/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace RecipesApi
 {
@@ -32,6 +34,20 @@ namespace RecipesApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RecipesApi", Version = "v1" });
             });
+            services
+                .AddAuthentication(opt => {
+                    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options => {
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters() {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidAudience = "https://localhost:5001",
+                        ValidIssuer = "https://localhost:5001",
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +65,8 @@ namespace RecipesApi
             app.UseStatusCodePages();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
