b9053ca2c04c12d63c998c6012eefaeeeb17e943
OmkarKandale <omkarkandale@gmail.com>
2021-08-29 14:12:42 +0530

    added issuersigningKey

--- RecipesApi/Startup.cs
+++ RecipesApi/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -46,6 +47,7 @@ namespace RecipesApi
                         ValidateAudience = true,
                         ValidAudience = "https://localhost:5001",
                         ValidIssuer = "https://localhost:5001",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("keyboard_cat"))
                     };
                 });
         }
