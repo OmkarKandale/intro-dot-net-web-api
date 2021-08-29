b3cf40c76d0c7f04e7662ada4a315faf8570516e
OmkarKandale <omkarkandale@gmail.com>
2021-08-29 23:34:17 +0530

    added swagger configuration

--- RecipesApi/Startup.cs
+++ RecipesApi/Startup.cs
@@ -31,9 +31,29 @@ namespace RecipesApi
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+            services.AddSwaggerGen(swagger =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RecipesApi", Version = "v1" });
+                swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "RecipesApi", Version = "v1" });
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme() {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement{
+                    {
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
             });
             services
                 .AddAuthentication(opt => {
@@ -45,9 +65,10 @@ namespace RecipesApi
                     options.TokenValidationParameters = new TokenValidationParameters() {
                         ValidateIssuer = true,
                         ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
                         ValidAudience = "https://localhost:5001",
                         ValidIssuer = "https://localhost:5001",
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("keyboard_cat"))
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("really_big_keyboard_cat"))
                     };
                 });
         }
